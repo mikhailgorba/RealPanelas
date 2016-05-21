2545b06579eeca684f6a898ea29b9f6c74877c9a
mikhailgorba <mikhailgorba@hotmail.com>
2016-05-21 18:51:59 -0300

    correçao de erros
    index correção
    aplication.ini correçao

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
-<<<<<<< HEAD
+//<<<<<<< HEAD
 
 session_start();
 
-=======
-session_start();
->>>>>>> 6bfecd6201a90cde0606afdb75b849a2fdfcde08
+//=======
+//session_start();
+//>>>>>>> 6bfecd6201a90cde0606afdb75b849a2fdfcde08
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
