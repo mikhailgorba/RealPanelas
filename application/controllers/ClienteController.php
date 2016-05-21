2704b467f6654209371cf2b728efd576680a76c8
mikhailgorba <mikhailgorba@hotmail.com>
2016-05-21 19:17:17 -0300

    Update  Cliente
    criei a nova action GRAVAR

--- application/controllers/ClienteController.php
+++ application/controllers/ClienteController.php
@@ -4,13 +4,18 @@ class ClienteController extends Zend_Controller_Action
 {
 
     public function formularioAction()
-    {
-       
+    {                                  
+      
     }
 
     public function listarAction()
     {
        
+    }
+    
+    public function gravarAction()
+    {
+       $dados = $this->_getAllParams();  
     }
 
 }
