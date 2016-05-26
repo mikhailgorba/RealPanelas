919f5d440742a97d74f35bbeb98927a324db8f9b
sandro augusto andrade <sandro_andrade2008@hotmail.com>
2016-05-26 16:24:01 -0300

    produto
    GRAVAR E EXCLUIR

--- application/controllers/ProdutoController.php
+++ application/controllers/ProdutoController.php
@@ -13,9 +13,26 @@ class ProdutoController extends Zend_Controller_Action
         
     }
     
+    public function excluirAction()
+	{
+        $dados = $this->_getAllParams();
+		$modelProduto = new Application_Model_Produto();
+		
+		$modelProduto->excluir($dados);
+		
+		$this->redirect('produto/listar');
+		
+	}
+    
     public function gravarAction()
     {
+      $dados = $this->_getAllParams();
+        $modelProduto = new Application_Model_Produto();
         
+
+        $modelProduto->gravar($dados);
+        
+        $this->redirect('produto/listar');  
     }
 
 }
