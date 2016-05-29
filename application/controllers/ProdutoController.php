27d2f6adc3afec482b3921b0a3fe6e24df5f8c30
carlitofilmagens <carlitofilmagens@hotmail.com>
2016-05-29 15:43:51 -0300

    upLoad
    Criado uoLoad em ControlllerProdutos.

--- application/controllers/ProdutoController.php
+++ application/controllers/ProdutoController.php
@@ -3,14 +3,26 @@
 class ProdutoController extends Zend_Controller_Action
 {
 
+    public function upLoadAction()
+    {
+     
+    }
+    
     public function pesquisarAction()
     {
      
     }
 
-    public function formularioAction()
-    {
-        
+           public function formularioAction()
+   {
+        $dados = $this->_getAllParams();
+		
+		$modelProduto = new Application_Model_Produto();
+		
+		if(!empty($dados['id_produto'])){
+			$row = $modelProduto->fetchRow('id_produto = ' . $dados['id_produto']);
+		} else {
+			$row = $modelProduto->createRow();
     }
     
     public function listarAction(){
@@ -47,5 +59,54 @@ class ProdutoController extends Zend_Controller_Action
     }
 
 }
+_______________________________________
+    class ClienteController extends Zend_Controller_Action
+{
 
+    public function formularioAction()
+   {
+        $dados = $this->_getAllParams();
+		
+		$modelCliente = new Application_Model_Cliente();
+		
+		if(!empty($dados['id_clientes'])){
+			$row = $modelCliente->fetchRow('id_clientes = ' . $dados['id_clientes']);
+		} else {
+			$row = $modelCliente->createRow();
+		}
+		
+		$this->view->row = $row;
+	}
+
+    public function listarAction()
+    {
+        $modelCliente = new Application_Model_Cliente();
+		
+		$rowSet = $modelCliente->fetchAll();
+		$this->view->rowSet = $rowSet; 
+    }
+    
+    public function gravarAction()
+    {
+       $dados = $this->_getAllParams(); 
+        
+       $modelCliente = new Application_Model_Cliente();
+        
+        $modelCliente->gravar($dados);
+        
+        $this->redirect('cliente/listar');
+        
+    }
+
+    public function excluirAction()
+	{
+        $dados = $this->_getAllParams();
+		$modelCliente = new Application_Model_Cliente();
+		
+		$modelCliente->excluir($dados);
+		
+		$this->redirect('cliente/listar');
+		
+	}
+}
  
