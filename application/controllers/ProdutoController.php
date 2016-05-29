c98d79d227d0940e414c0405d63f80bcb54f332b
mikhailgorba <mikhailgorba@hotmail.com>
2016-05-29 19:03:04 -0300

    tentativas fotos

--- application/controllers/ProdutoController.php
+++ application/controllers/ProdutoController.php
@@ -7,6 +7,8 @@ class ProdutoController extends Zend_Controller_Action
     {
       $dados = $this->_getAllParams();
       $this->view->id_produto = $dados['id_produto']; 
+    
+        
     }
     
     public function pesquisarAction()
@@ -43,13 +45,19 @@ class ProdutoController extends Zend_Controller_Action
 		
 	}
     
-    public function salvarFotoAction()
+    public function salvarAction() //salvar foto
 	{
         $dados = $this->_getAllParams(); 
         $modelFoto = new Application_Model_Foto();
+        $produto_id_produto = $dados['produto_id_produto'];
 
         $modelFoto->gravar($dados);
         
+        $foto = $this->uploadFoto($produto_id_produto);
+        
+        $dadosUpdate['nome'] = $foto;
+        $modelFoto->update($dadosUpdate, "produto_id_produto = $produto_id_produto");
+
         $this->redirect('produto/pesquisar'); 
 		
 	}
@@ -61,13 +69,20 @@ class ProdutoController extends Zend_Controller_Action
 
         $id_produto = $modelProduto->gravar($dados);
         
-   
-        //$row = $modelProduto->fetchRow('id_produto = ' . $dados['id_produto']);
-
-        //$this->view->row = $row;
-       
-
         $this->redirect('produto/upload/id_produto/' . $id_produto);  
     }
+    
+    public function uploadFoto($produto_id_produto)
+	{
+        if(isset($_FILES['foto'])&& $_FILES['foto']['error'] == 0){
+        
+            $origem = $_FILES['foto']['tmp_name'];
+            $extensao = substr($_FILES['foto']['name'], strrpos($_FILES['foto']['name'], '.'));
+            $destino = 'img/fotos/foto_' . $produto_id_produto . $extensao;
+            
+            move_uploaded_file($origem, $destino);
+            return $id_produto . $extensao;
+        }
+	}
    
  }
