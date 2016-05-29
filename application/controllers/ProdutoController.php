8269823a13cc408307377dffa3be2d9aa239ea70
carlitofilmagens <carlitofilmagens@hotmail.com>
2016-05-29 15:53:54 -0300

    correcao
    controller produto

--- application/controllers/ProdutoController.php
+++ application/controllers/ProdutoController.php
@@ -24,6 +24,8 @@ class ProdutoController extends Zend_Controller_Action
 		} else {
 			$row = $modelProduto->createRow();
     }
+        $this->view->row = $row;
+	}
     
     public function listarAction(){
         
@@ -33,6 +35,18 @@ class ProdutoController extends Zend_Controller_Action
 		$this->view->rowSet = $rowSet; 
     }
     
+        
+    public function gravarAction()
+    {
+       $dados = $this->_getAllParams(); 
+        
+       $modelCliente = new Application_Model_Cliente();
+        
+        $modelCliente->gravar($dados);
+        
+        $this->redirect('cliente/listar');
+        
+        
     public function excluirAction()
 	{
         $dados = $this->_getAllParams();
@@ -61,30 +75,7 @@ class ProdutoController extends Zend_Controller_Action
 }
 _______________________________________
     class ClienteController extends Zend_Controller_Action
-{
 
-    public function formularioAction()
-   {
-        $dados = $this->_getAllParams();
-		
-		$modelCliente = new Application_Model_Cliente();
-		
-		if(!empty($dados['id_clientes'])){
-			$row = $modelCliente->fetchRow('id_clientes = ' . $dados['id_clientes']);
-		} else {
-			$row = $modelCliente->createRow();
-		}
-		
-		$this->view->row = $row;
-	}
-
-    public function listarAction()
-    {
-        $modelCliente = new Application_Model_Cliente();
-		
-		$rowSet = $modelCliente->fetchAll();
-		$this->view->rowSet = $rowSet; 
-    }
     
     public function gravarAction()
     {
