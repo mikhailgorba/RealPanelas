b71dffff48d735a326fa2c9769e2a97582ae15d3
mikhailgorba <mikhailgorba@hotmail.com>
2016-05-29 17:28:36 -0300

    Upload Fotos id

--- application/controllers/ProdutoController.php
+++ application/controllers/ProdutoController.php
@@ -5,7 +5,8 @@ class ProdutoController extends Zend_Controller_Action
 
     public function uploadAction()
     {
-     
+      $dados = $this->_getAllParams();
+      $this->view->id_produto = $dados['id_produto']; 
     }
     
     public function pesquisarAction()
@@ -42,20 +43,31 @@ class ProdutoController extends Zend_Controller_Action
 		
 	}
     
+    public function salvarFotoAction()
+	{
+        $dados = $this->_getAllParams(); 
+        $modelFoto = new Application_Model_Foto();
+
+        $modelFoto->gravar($dados);
+        
+        $this->redirect('produto/pesquisar'); 
+		
+	}
+    
     public function gravarAction()
     {
         $dados = $this->_getAllParams(); 
         $modelProduto = new Application_Model_Produto();
 
-        $modelProduto->gravar($dados);
+        $id_produto = $modelProduto->gravar($dados);
         
-        if(!empty($dados['id_produto'])){
-			$row = $modelProduto->fetchRow('id_produto = ' . $dados['id_produto']);
-		} else {
-			$row = $modelProduto->createRow();
-        $this->view->row = $row;
-        
+   
+        //$row = $modelProduto->fetchRow('id_produto = ' . $dados['id_produto']);
 
+        //$this->view->row = $row;
+       
+
+        $this->redirect('produto/upload/id_produto/' . $id_produto);  
     }
    
  }
